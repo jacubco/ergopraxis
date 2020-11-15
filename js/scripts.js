996d65a5ffa7fa0667f318f301af4a2929205cf2
xy <xy@zz.com>
2020-11-15 21:57:52 +0100

    initialize repository

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+console.log('oh my heavens');
+
+window.addEventListener('scroll', addStickyNav);
+
+// function addStickyNav(e) {
+//   var navBar = document.querySelector('.navbar');
+//   var pixels = 150;
+//   var pixelsScrolled = window.scrollY;
+
+//   if (pixelsScrolled > pixels) {
+//     navBar.classList.add('navbar--sticky');
+//   }
+//   else {
+//     navBar.classList.remove('navbar--sticky');
+//   } 
+// }
